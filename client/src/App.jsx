fbd91f40362f9e16fbee4ee4b190779a62e219d6
mahika1306 <mahikadhar1@gmail.com>
2023-10-11 01:24:36 +0530

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/signin';
+import SignUp from './pages/signUp';
+import About from './pages/about';
+import Profile from './pages/profile';
+import Header from './components/header';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+     <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
+ 
+  )
+}
